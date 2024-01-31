e12e97a5e1717f384f61ce7ef70fd67a7e7a22a6
Your Name <you@example.com>
2024-01-31 13:34:23 +0200

    apartments

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ apartments = []
 
 # https://www.w3schools.com/python/python_file_handling.asp
 # https://www.w3schools.in/python/file-handling
-with open('jurmala.csv', newline='') as csv_file:
-    file_reader = csv.reader(csv_file, delimiter = ',', quotechar='|')
+with open('jurmala.csv', newline='', encoding='utf-8') as csv_file:
+    file_reader = csv.reader(csv_file, delimiter = ',', quotechar='"')
 
     for row in file_reader:
         apartments.append(row)
@@ -14,7 +14,7 @@ with open('jurmala.csv', newline='') as csv_file:
 # remove header row
 apartments.pop(0)
 
-# print(apartments)
+#print(apartments[0][8])
 
 while True:
     print("1. Get apartments by sequence number")
@@ -28,17 +28,34 @@ while True:
     choice = input("Enter your choice (1-6): ")
 
     if choice == '1':
-        # https://www.w3schools.com/python/python_lists_access.asp
-        pass
+        number = input('Whats the sequence number?: ')
+        print(apartments[int(number)])
     elif choice == '2':
-        # https://www.w3schools.com/python/python_lists_sort.asp
+        def myfunc(apartment):
+            return int(apartment[8])
+        apartments.sort(key = myfunc, reverse = True)
+        print(apartments[0:9])
         pass
     elif choice == '3':
-        # https://www.w3schools.com/python/python_lists_sort.asp
+        def myfunc(apartment):
+            return int(apartment[8])
+        apartments.sort(key = myfunc)
+        print(apartments[0:9])
         pass
     elif choice == '4':
         # https://www.w3schools.com/python/python_lists_comprehension.asp
         # https://www.w3schools.com/python/python_lists_access.asp - Range of Indexes
+        newprice = []
+        nameprice = int(input('name a price: '))
+
+        def myfunc(apartment):
+            return int(apartment[8])
+        apartments.sort(key = myfunc, reverse = True)
+        
+        for x in apartments:
+            if nameprice > int(x[8]):
+                newprice.append(x)
+        print(newprice[0:19])
         pass
     elif choice == '5':
         # https://www.w3schools.com/python/python_lists_comprehension.asp
