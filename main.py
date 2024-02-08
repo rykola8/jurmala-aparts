ce263d1f2a88619233c158c6c12f04d9ef4a5ea1
Your Name <you@example.com>
2024-02-08 17:32:04 +0200

    jurmala rooms

--- main.py
+++ main.py
@@ -14,7 +14,11 @@ with open('jurmala.csv', newline='', encoding='utf-8') as csv_file:
 # remove header row
 apartments.pop(0)
 
-#print(apartments[0][8])
+def print_apartments(apartments):
+    for apartment in apartments:
+        print(apartment[2])
+        print(apartment[8], "euro")
+        print("=============")
 
 while True:
     print("1. Get apartments by sequence number")
@@ -22,7 +26,7 @@ while True:
     print("3. Top 10 by lowest price")
     print("4. 20 items, cheaper than price")
     print("5. 20 items, expensive than price")
-    print("6. -- Filter of your choice --")
+    print("6. Apartments with the right number of rooms")
     print("7. Exit")
 
     choice = input("Enter your choice (1-6): ")
@@ -34,13 +38,13 @@ while True:
         def myfunc(apartment):
             return int(apartment[8])
         apartments.sort(key = myfunc, reverse = True)
-        print(apartments[0:9])
+        print_apartments(apartments[0:9])
         pass
     elif choice == '3':
         def myfunc(apartment):
             return int(apartment[8])
         apartments.sort(key = myfunc)
-        print(apartments[0:9])
+        print_apartments(apartments[0:9])
         pass
     elif choice == '4':
         # https://www.w3schools.com/python/python_lists_comprehension.asp
@@ -55,15 +59,35 @@ while True:
         for x in apartments:
             if nameprice > int(x[8]):
                 newprice.append(x)
-        print(newprice[0:19])
+        print_apartments(newprice[0:19])
         pass
     elif choice == '5':
-        # https://www.w3schools.com/python/python_lists_comprehension.asp
-        # https://www.w3schools.com/python/python_lists_access.asp - Range of Indexes
+        newprice = []
+        nameprice = int(input('name a price: '))
+
+        def myfunc(apartment):
+            return int(apartment[8])
+        apartments.sort(key = myfunc)
+        
+        for x in apartments:
+            if nameprice < int(x[8]):
+                newprice.append(x)
+        print(newprice[0:19])
         pass
 
     elif choice == '6':
-        # 
+        new_room = []
+        room = input("number of rooms: ")
+
+        print(apartments[0])
+
+        for x in apartments:
+            
+            if x[4] == room:
+                new_room.append(x)
+         
+        print(new_room)
+
         pass
     elif choice == '7':
         print("Exiting")
